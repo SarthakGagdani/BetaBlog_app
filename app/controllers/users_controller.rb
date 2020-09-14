816f6a72b7863948d4e7a5cc962f6d710b99373b
Sarthak Gagdani <sarthak.gag@gmail.com>
2020-09-14 15:06:13 +0530

    Added Pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,33 @@
 class UsersController < ApplicationController 
 
+    before_action :set_user, only:[:show, :edit, :update]
+
     def new
         @user=User.new
     end
+
+    def show
+        @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+    end
+
+    def index
+        @users = User.paginate(page: params[:page], per_page: 5)
+
+    end
+
+
+    def edit
+    end
+    
+    def update
+        if @user.update(user_params)
+            flash[:notice] = "Your account information was successfully updated"
+            redirect_to @user
+        else
+            render 'edit'
+        end
+    end
+
 
     def create
         @user = User.new(user_params)
@@ -15,6 +40,10 @@ class UsersController < ApplicationController
     end
     
     private
+    def set_user
+        @user = User.find(params[:id])
+    end
+
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end
